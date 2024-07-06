e5861f9aa3cd5203e7e80c5384a275dc34548aad
Paul Boldienkov <you@example.com>
2024-07-06 17:22:47 +0200

    Initial spring boot app has been created

---
+++ src/main/java/com/banking/secured_banking_app/SecuredBankingAppApplication.java
@@ -0,0 +1,16 @@
+package com.banking.secured_banking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SecuredBankingAppApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SecuredBankingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/secured_banking_app/SecuredBankingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.banking.secured_banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SecuredBankingAppApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
