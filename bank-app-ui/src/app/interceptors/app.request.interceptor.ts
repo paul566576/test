8ee157914d9149ab2753825de1146311e7e28bfd
paul566576 <slimm566576@gmail.com>
2024-09-04 13:34:04 +0200

    Now XSRF token data is added to session storage, cookies and headers

--- bank-app-ui/src/app/interceptors/app.request.interceptor.ts
+++ bank-app-ui/src/app/interceptors/app.request.interceptor.ts
@@ -19,6 +19,13 @@ export class XhrInterceptor implements HttpInterceptor {
       httpHeaders = httpHeaders.append('Authorization', 'Basic ' + window.btoa(this.user.email + ':' + this.user.password));
     }
 
+    let xsrf = sessionStorage.getItem('XSRF-TOKEN');
+
+    if(xsrf) {
+      httpHeaders.append('X-XSRF-TOKEN', xsrf);
+    }
+
+
     httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
     const xhr = req.clone({
       headers: httpHeaders
