7ffe43cf2805a7440e4fb807508f7177ef0e38b6
paul566576 <slimm566576@gmail.com>
2024-09-29 19:13:25 +0200

    Accounts microservice has been created

---
+++ accounts/src/test/java/com/banking/accounts/AccountsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.banking.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class AccountsApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
