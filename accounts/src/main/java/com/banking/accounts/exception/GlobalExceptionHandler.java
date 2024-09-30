4b8925502f9ad7051d8d7559aed7f9ea0eac4643
paul566576 <slimm566576@gmail.com>
2024-09-30 20:36:53 +0200

    GlobalExceptionHandler has been created

---
+++ accounts/src/main/java/com/banking/accounts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.banking.accounts.exception;
+
+import com.banking.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler
+{
+	@ExceptionHandler(value = { CustomerAlreadyExistsException.class })
+	public ResponseEntity<ErrorResponseDto> handleException(final CustomerAlreadyExistsException exception,
+			final WebRequest request)
+	{
+		final ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+				request.getDescription(false),
+				HttpStatus.BAD_REQUEST,
+				exception.getMessage(),
+				LocalDateTime.now()
+		);
+
+		return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+	}
+}
