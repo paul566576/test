37a046729907898e331f923c20064a4d3c4a5a67
paul566576 <slimm566576@gmail.com>
2024-09-30 20:35:58 +0200

    Customer and Account repositories have been created

---
+++ accounts/src/main/java/com/banking/accounts/repository/AccountsRepository.java
@@ -0,0 +1,11 @@
+package com.banking.accounts.repository;
+
+import com.banking.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long>
+{
+}
