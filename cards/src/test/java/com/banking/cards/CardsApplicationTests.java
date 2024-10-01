54eeb6326a71faf376f556f5e283707242f4e997
paul566576 <slimm566576@gmail.com>
2024-10-01 14:21:22 +0200

    Empty cards microservice has been created

---
+++ cards/src/test/java/com/banking/cards/CardsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.banking.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CardsApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
