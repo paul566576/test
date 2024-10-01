5aececbcc067bac7afc95f44c32dfb2c9dd8a60a
paul566576 <slimm566576@gmail.com>
2024-10-01 15:48:41 +0200

    GlobalExceptionHandler has been added to Cards MS

--- cards/src/main/java/com/banking/cards/dto/ErrorResponseDto.java
+++ cards/src/main/java/com/banking/cards/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.banking.cards.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 
 
 @Data
+@AllArgsConstructor
 public class ErrorResponseDto
 {
 	private String apiPath;
